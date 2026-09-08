bacce0778339102dc2832dc39279c34719aed6eb
jettspanner123 <uddeshyasingh12bsci@gmail.com>
2026-09-08 14:04:12 +0530

    Fixed: Multi-DataSource Configuration And Application Defaults For Render Deployment

--- SignForgeOrchestratorServiceLayerMSC/src/main/java/com/theweplm/signforge/Config/AssetSphereDatabaseConfiguration.java
+++ SignForgeOrchestratorServiceLayerMSC/src/main/java/com/theweplm/signforge/Config/AssetSphereDatabaseConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
@@ -13,6 +14,35 @@ import javax.sql.DataSource;
 
 @Configuration
 public class AssetSphereDatabaseConfiguration {
+
+    // ==========================================
+    // Primary SignForge DataSource Configuration
+    // ==========================================
+
+    @Primary
+    @Bean(name = "dataSourceProperties")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean(name = "dataSource")
+    public DataSource dataSource(@Qualifier("dataSourceProperties") DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Primary
+    @Bean(name = "jdbcTemplate")
+    public JdbcTemplate jdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    // ============================================
+    // Secondary AssetSphere DataSource Configuration
+    // ============================================
 
     @Bean(name = "assetSphereDataSourceProperties")
     @ConfigurationProperties(prefix = "assetsphere.datasource")
